a6a40a2ff0a6787b2b1af0e6d3012b25fae92e33
shrestha-ramesh <shresthashare@gmail.com>
2022-10-30 16:59:30 -0500

    missing build gradle file

---
+++ module-1/src/main/java/com/user/module1/controller/Home.java
@@ -0,0 +1,27 @@
+package com.user.module1.controller;
+
+import com.user.common.model.Product;
+import com.user.common.model.Products;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class Home {
+
+
+    @GetMapping("/module1")
+    public Products Home(){
+        List<Product> listProduct = new ArrayList<>();
+        Product a = new Product(1,"noodle","ramen");
+        Product b = new Product(2,"noodle","ramen");
+        Product c = new Product(3,"noodle","ramen");
+        listProduct.add(a);
+        listProduct.add(b);
+        listProduct.add(c);
+        Products products = Products.builder().products(listProduct).build();
+        return products;
+    }
+}
